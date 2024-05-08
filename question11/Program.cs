5494f38ea924826f3c544c73c8a9d60eda2e0618
egeklcsln <94693997+egeklcsln@users.noreply.github.com>
2024-05-08 20:28:11 +0300

    Add files via upload

---
+++ question11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        string output = ReverseString(input);
+
+        Console.WriteLine("Reversed string:");
+        Console.WriteLine(output);
+    }
+
+    static string ReverseString(string input)
+    {
+        // Split the input string into words and remove empty entries
+        string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Reverse the order of words
+        Array.Reverse(words);
+
+        // Join the words back into a single string with trimmed spaces
+        string reversedString = string.Join(" ", words);
+
+        return reversedString;
+    }
+}
